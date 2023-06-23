40ba32c76bfa8d5264bc54f1bf71dbeadd11bdc2
Hà Trung Nghĩa <115788443+HTNghia0503@users.noreply.github.com>
2023-06-23 21:29:19 +0700

    Update file sql

--- order_management.sql
+++ order_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 22, 2023 at 08:37 PM
+-- Generation Time: Jun 23, 2023 at 04:25 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
